01a5a94c59a353a4ceff26b1d497ef860fdc9936
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-08-12 14:38:42 +0900

    user.items.view調整

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,1 +1,26 @@
-商品一覧
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            ホーム
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    @foreach ($products as $product)
+                        <div class="w-1/4 p-2 md:p-4">
+                            <a href="">
+                                <x-thumbnail filename="{{ $product->imageFirst->filename ?? '' }}" type="products" />
+                                <div class="text-gray-700">
+                                    {{ $product->name }}
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
